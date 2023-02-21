b5e4c4b5c92fea7f8ec4d816259757c318b539a4
Julio Radilla <Itsdigitaljulio@gmail.com>
2023-02-20 22:45:02 -0600

    Daily Katas

---
+++ codewars8kyu/grasshopperMessiGoalsFunction.js
@@ -0,0 +1,16 @@
+/*Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17*/
+//Solution:
+const  goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals)  =>  laLigaGoals + copaDelReyGoals + championsLeagueGoals 
+//Use arrow function to make it a 1 line code and will add each parameter to get the sum of goals Messi scored
