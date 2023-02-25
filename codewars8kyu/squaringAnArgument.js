12994cebdc83e26f71a4d0805d1d72b79cec4b40
Julio Radilla <Itsdigitaljulio@gmail.com>
2023-02-24 20:27:02 -0600

    Daily Katas

---
+++ codewars8kyu/squaringAnArgument.js
@@ -0,0 +1,6 @@
+/*Now you have to write a function that takes an argument and returns the square of it.*/
+//Solution:
+const square = num => Math.pow(num,2)
+//Use an arrow function and use 1 parameter to let the user input a number
+//use instant return by not putting curly braces
+//Use Math.pow() to square the argument 
