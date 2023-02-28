fe8d58f201d3d4a3e8a0ba7407d4d1d470e781c0
Julio Radilla <Itsdigitaljulio@gmail.com>
2023-02-27 21:38:05 -0600

    Daily Katas

---
+++ codewars8kyu/basicVariableAssigment.js
@@ -0,0 +1,6 @@
+/*This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?*/
+//Solution:
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+//Use only one equal sign to assign a value to a variable
