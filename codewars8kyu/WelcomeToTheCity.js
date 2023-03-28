dbd210edd22fad7d355e262c67bf9e01dbe39861
Julio Radilla <Itsdigitaljulio@gmail.com>
2023-03-28 17:18:12 -0500

    Daily Kata for my birthday

---
+++ codewars8kyu/WelcomeToTheCity.js
@@ -0,0 +1,13 @@
+/*Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space between each, and the length of the name array in test cases will vary.
+
+Example:
+
+sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')
+This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!*/
+//Solution:
+function sayHello( name, city, state ) {
+    //Get the array given in name and use join method and put it in a variable
+    let names = name.join(' ')
+    //return it with template strings
+    return `Hello, ${names}! Welcome to ${city}, ${state}!`;
+}
