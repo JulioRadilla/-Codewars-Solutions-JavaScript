46361fd47d4ea36e5b38bc9d83dd00b8e6cf0461
JulioRadilla <102002213+JulioRadilla@users.noreply.github.com>
2023-04-22 23:43:13 +0000

    Daily Katas

---
+++ codewars8kyu/kataExampleTwist.js
@@ -0,0 +1,7 @@
+/*This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites 1,000 times.*/
+//Solution:
+//Use the new Array syntax and put 1000 so that it returns an array with that length inputed
+//Use the fill method on the array created and use the codewars value so it can fill the whole array length created 
+var websites = new Array(1000).fill("codewars")
