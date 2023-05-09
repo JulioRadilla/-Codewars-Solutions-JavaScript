577f345766e8f0e38516816209c29474dc1b7873
JulioRadilla <102002213+JulioRadilla@users.noreply.github.com>
2023-05-09 23:38:09 +0000

    Daily Katas

---
+++ codewars8kyu/5WithoutNumbers.js
@@ -0,0 +1,12 @@
+/*Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)*/
+//Solution:
+function unusualFive() {
+    //Use a variable to hold in a string that has 5 characters
+    let five = 'Five!';
+    //return the length of the string
+    return five.length;
+}
